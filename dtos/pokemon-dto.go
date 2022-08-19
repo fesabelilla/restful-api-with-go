3bc250da5672cfa08244f5e7b5dcb11b6b6a0242
fesabelilla <fesabelilla@gmail.com>
2022-08-19 20:24:43 +0600

    Mapped JSON value to an Object

--- dtos/pokemon-dto.go
+++ dtos/pokemon-dto.go
@@ -1,1 +1,15 @@
 package dtos
+
+type Response struct {
+	Name    string    `json:"name"`
+	Pokemon []Pokemon `json:"pokemon_entries"`
+}
+
+type Pokemon struct {
+	EntryNo int            `json:"entry_number"`
+	Species PokemonSpecies `json:"pokemon_species"`
+}
+
+type PokemonSpecies struct {
+	Name string `json:"name"`
+}
