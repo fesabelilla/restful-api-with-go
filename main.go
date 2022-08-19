f460a9e58e1934630aca3b98f1c8cf4c2de770bf
fesabelilla <fesabelilla@gmail.com>
2022-08-19 12:40:58 +0600

    Restful api -

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import restful_api "restful-api-with-go/restful-api"
+
+func main() {
+	restful_api.GetResponse()
+}
---
+++ restful-api/get-api.go
@@ -0,0 +1,28 @@
+package restful_api
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func GetResponse() {
+
+	responses, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
+
+	if err != nil {
+		log.Fatal("Error occurred during Get API response")
+		os.Exit(1)
+	}
+
+	//ioutil.ReadAll(response.Body) to read in data from the incoming byte stream
+	responseData, err := ioutil.ReadAll(responses.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// convert this []byte response into a string using string(responseData)
+	fmt.Println(string(responseData))
+}
